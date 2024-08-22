1c1af42da011cb8a118c19d411896616bf6ed29d
Beatriztesser <beatrizeduardabt@gmail.com>
2024-08-22 11:28:43 -0400

    apii

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app= express();
+
+app.get('/', (req, res)=> {
+    let mensagem= ({mensagem: 'Hello world'});
+    res.json(mensagem);
+
+});
+
+app.listen(8080,()=>{
+    let data= new Date();
+    console.log('Servidor node iniciado em: '+data)
+});
--- helloworld.js
+++
@@ -1,2 +0,0 @@
-console.log("Olá Mundo");
-//Primeira aula
